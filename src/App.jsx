00a9375a3f03ea03c724cbb243b969fd9e8e683d
vsemprivet2727 <dambosssss@gmail.com>
2026-02-05 13:24:32 +0300

    send add file function

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ function handleFileChange(event) {
   const files = event.target.files; // Получаем FileList
   if (files.length > 0) {
     const file = files[0];
-    console.log("Выбран файл:", file.name, file.size, file.type);
+    alert("Выбран файл:"+ file.name+ file.size+ file.type);
+  }
+}
+
+function sendFile() {
+  const inputImage = document.getElementById('imageLoader');
+  if (!imageLoader || !imageLoader.files<1) return;
+
+  const file = inputImage.files;
+  const formData = new FormData();
+  formData.append('image', file);
+
+  try {
+    // логика пост запроса
+  } catch (e) {
+    console.log(e.Message);
   }
 }
 
@@ -16,6 +31,7 @@ function App() {
         accept="image/*"
         multiple
         onChange={handleFileChange}
+        id='imageLoader'
       />
     </>
   );
